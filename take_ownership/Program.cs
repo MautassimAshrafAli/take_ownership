38498f526edb1968f2083541c6e5fdeaadfa79cf
Mutassim Ashraf Ali <79098078+MutassimAshraf123@users.noreply.github.com>
2021-02-23 21:15:22 +0200

    ownership

---
+++ take_ownership/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace take_ownership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ownership
+            String FileName = @"C:\Windows\SystemApps\Microsoft.Windows.PinningConfirmationDialog_cw5n1h2txyewy";
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+
+            startInfo.FileName = "cmd.exe";
+
+            startInfo.Arguments = @" /k takeown /f " + FileName;
+
+            process.StartInfo = startInfo;
+            process.Start();
+
+
+            System.Threading.Thread.Sleep(100);
+
+            System.Diagnostics.Process f_r = new System.Diagnostics.Process();
+            f_r.StartInfo.FileName = "cmd.exe";
+            f_r.StartInfo.RedirectStandardInput = true;
+            f_r.StartInfo.RedirectStandardOutput = true;
+            f_r.StartInfo.CreateNoWindow = true;
+            f_r.StartInfo.UseShellExecute = false;
+            f_r.Start();
+
+            f_r.StandardInput.WriteLine("cd/ & cd windows & cd system32 & cacls \"" + FileName + "\" /e /p Everyone:f");
+            f_r.StandardInput.Flush();
+            f_r.StandardInput.Close();
+        }
+    }
+}
